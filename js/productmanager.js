54f12df9b2896e147cd8a7acfa851de36d3bac4c
beykaarusignups-bit <beykaarusignups@gmail.com>
2026-07-10 19:57:35 +0500

    Update productmanager.js

--- js/productmanager.js
+++ js/productmanager.js
@@ -11,3 +11,29 @@ import {
     uploadBytes,
     getDownloadURL
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+
+const saveBtn = document.getElementById("saveProduct");
+
+const productName = document.getElementById("productName");
+
+const productBrand = document.getElementById("productBrand");
+
+const productCategory = document.getElementById("productCategory");
+
+const productPrice = document.getElementById("productPrice");
+
+const productStock = document.getElementById("productStock");
+
+const productDescription = document.getElementById("productDescription");
+
+const productFeatured = document.getElementById("productFeatured");
+
+const productImages = document.getElementById("productImages");
+
+saveBtn.addEventListener("click", saveProduct);
+
+async function saveProduct(){
+
+    alert("Save button connected successfully.");
+
+}
