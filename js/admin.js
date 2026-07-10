1b987b50cd047d582798fc63b5d2958da808639b
beykaarusignups-bit <beykaarusignups@gmail.com>
2026-07-10 20:28:47 +0500

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,27 +1,30 @@
+import { db, storage } from "./firebase.js";
+
+import {
+    collection,
+    addDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 const modal = document.getElementById("productModal");
-
 const addBtn = document.getElementById("addProductBtn");
-
 const closeBtn = document.getElementById("closeModal");
+const saveBtn = document.getElementById("saveProduct");
 
 addBtn.addEventListener("click", () => {
-
     modal.style.display = "flex";
-
 });
 
 closeBtn.addEventListener("click", () => {
-
     modal.style.display = "none";
-
 });
 
 window.addEventListener("click", (e) => {
-
     if (e.target === modal) {
-
         modal.style.display = "none";
-
     }
+});
 
+saveBtn.addEventListener("click", () => {
+    alert("Save button connected successfully.");
 });
