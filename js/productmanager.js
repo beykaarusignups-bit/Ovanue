c7282bb20c4e5f25057636d00e9c3c5e72865db8
beykaarusignups-bit <beykaarusignups@gmail.com>
2026-07-10 19:55:55 +0500

    Update productmanager.js

--- js/productmanager.js
+++ js/productmanager.js
@@ -1,1 +1,13 @@
+import { db, storage } from "./firebase.js";
 
+import {
+    collection,
+    addDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {
+    ref,
+    uploadBytes,
+    getDownloadURL
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
