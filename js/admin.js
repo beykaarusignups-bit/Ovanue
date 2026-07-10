3832f1012fd91e02b87a1def84063b36f675a1e0
beykaarusignups-bit <beykaarusignups@gmail.com>
2026-07-11 01:30:08 +0500

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,23 +1,64 @@
+import { db } from "./firebase.js";
+
+import {
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 const modal = document.getElementById("productModal");
 
 const addBtn = document.getElementById("addProductBtn");
 const closeBtn = document.getElementById("closeModal");
 const saveBtn = document.getElementById("saveProduct");
 
+const productName = document.getElementById("productName");
+
+// Modal
+
 addBtn.addEventListener("click", () => {
+
     modal.style.display = "flex";
+
 });
 
 closeBtn.addEventListener("click", () => {
+
     modal.style.display = "none";
+
 });
 
 window.addEventListener("click", (e) => {
+
     if (e.target === modal) {
+
         modal.style.display = "none";
+
     }
+
 });
 
-saveBtn.addEventListener("click", () => {
-    alert("Save button works!");
+// Test Save
+
+saveBtn.addEventListener("click", async () => {
+
+    try {
+
+        await addDoc(collection(db, "test"), {
+
+            name: productName.value
+
+        });
+
+        alert("Firestore Connected!");
+
+    }
+
+    catch(error){
+
+        console.error(error);
+
+        alert(error.message);
+
+    }
+
 });
