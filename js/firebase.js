6c450c7f96ebf691dac078efb3d207ce2a0980eb
beykaarusignups-bit <beykaarusignups@gmail.com>
2026-07-10 19:55:06 +0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,32 +2,42 @@
 // Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-import {
-    getStorage
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
 
-import {
-    getAuth
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 const firebaseConfig = {
+
     apiKey: "AIzaSyCMw3mC9JoY9qPh_mZzEtDIFj1zKJkliAc",
+
     authDomain: "ovanue.firebaseapp.com",
+
     projectId: "ovanue",
+
     storageBucket: "ovanue.firebasestorage.app",
+
     messagingSenderId: "24477205910",
-    appId: "1:24477205910:web:f884358a83242b46fa49b7",
-    measurementId: "G-G0ZR9CVK8L"
-  };
+
+    appId: "1:24477205910:web:f884358a83242b46fa49b7"
+
+};
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+const db = getFirestore(app);
 
-export const storage = getStorage(app);
+const storage = getStorage(app);
 
-export const auth = getAuth(app);
+const auth = getAuth(app);
+
+export {
+
+    db,
+
+    storage,
+
+    auth
+
+};
